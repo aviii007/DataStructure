3cb44a81032fafe4476abf2e2c5a87fecc257514
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-25 18:08:22 +0530

    Added the welcome message.

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Linked list program");
+
+    }
+}
+
+
+
+
