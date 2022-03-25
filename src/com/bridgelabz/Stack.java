64e4640048818ee92f5aec6fe1cd782cd87fcb95
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-25 19:05:47 +0530

    Added the welcome message for stack and queue.

--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -3,9 +3,4 @@ package com.bridgelabz;
 public class Stack {
     // LIFO
     LinkedList list = new LinkedList();
-
-    // Push : Adding elements
-    public void push(String data) {
-        list.addFirst((String) data);
-    }
 }
