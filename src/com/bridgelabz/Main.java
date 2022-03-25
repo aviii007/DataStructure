63405de68283d7890d8fd200c77cb62ff3c0515b
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-25 19:12:02 +0530

    Added the Stack Pop and showing the elements.

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,5 +3,20 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Stacks and Queues Program");
+        System.out.println("STACK");
+        Stack stack = new Stack();
+        stack.push("ashvini");
+        stack.push("is");
+        stack.push("name");
+        stack.push("my");
+
+        stack.pop(); //will remove is
+        stack.showElements();
+        stack.pop(); //will remove name
+        stack.showElements();
+        stack.pop(); //will remove my
+        stack.showElements();
+        System.out.println("----------------");
+
     }
 }
--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -3,4 +3,17 @@ package com.bridgelabz;
 public class Stack {
     // LIFO
     LinkedList list = new LinkedList();
+    // Push : Adding elements
+    public void push(String data) {
+        list.addFirst((String) data);
+    }
+    // Pop : Deleting elements
+    public void pop() {
+        list.deleteFirst();
+    }
+
+    // TO display
+    public void showElements() {
+        list.printList();
+    }
 }
